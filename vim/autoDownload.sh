f631921db6374cf0c4f28cf582b246b9f46e5c2a
jincheng_liu <jincheng_liu@htc.com>
2015-01-25 03:00:35 +0000

    add script for vim env

---
+++ vim/autoDownload.sh
@@ -0,0 +1,44 @@
+#/bin/sh
+
+mkdir -p ~/.vim
+cp .vimrc ~/.vimrc
+
+#gitv
+git clone https://github.com/gregsexton/gitv.git
+cp -rf gitv/* ~/.vim
+
+#DirDiff
+git clone https://github.com/vim-scripts/DirDiff.vim.git
+cp -rf DirDiff.vim/* ~/.vim
+
+#genutils
+git clone https://github.com/vim-scripts/genutils.git
+cp -rf genutils/* ~/.vim
+
+#grep
+git clone https://github.com/vim-scripts/grep.vim.git
+cp -rf grep.vim/* ~/.vim
+
+#lookupfile
+git clone https://github.com/vim-scripts/lookupfile.git
+cp -rf lookupfile/* ~/.vim
+
+#minibufexpl
+git clone https://github.com/fholgado/minibufexpl.vim.git
+cp -rf minibufexpl.vim/* ~/.vim
+
+#vim-fugitive,  a Git wrapper
+git clone https://github.com/nevans/vim-fugitive.git
+cp -rf vim-fugitive/* ~/.vim
+
+#taglist
+git clone https://github.com/vim-scripts/taglist.vim.git
+cp -rf taglist.vim/* ~/.vim
+
+#winmanager
+git clone https://github.com/vim-scripts/winmanager.git
+cp -rf winmanager/* ~/.vim
+
+#netrw
+git clone https://github.com/vim-scripts/netrw.vim.git
+cp -rf netrw.vim/* ~/.vim
